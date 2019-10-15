2d5cbc28f0c1c455a23728167a23b1c65c63b3dc
Robbert van Mourik <robbert@webstack.nl>
2019-10-15 12:45:43 +0200

    Initial work

---
+++ ApiPlatformUuidFilterBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webstack\ApiPlatformUuidFilterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ApiPlatformUuidFilterBundle extends Bundle
+{
+
+}
